6a5c782517b0a014d7d3c63a096b98f91e8105da
Raul Gomez <rgomezbe@cisco.com>
2021-08-23 18:06:26 -0500

    Add devices file and video2 python scripts

--- video2_cap.py
+++ video2_cap.py
@@ -36,3 +36,4 @@ if __name__ == '__main__':
     for line in cap:
         print(line)
     print('')
+    
--- video2_conf.py
+++ video2_conf.py
@@ -34,5 +34,3 @@ if __name__ == '__main__':
     print('Aqui la configuracion: ')
     print(config)
     print('')
-
-    
