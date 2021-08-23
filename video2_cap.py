9f63cf5b6277fdce2a83be0420d3f52256e10047
Raul Gomez <rgomezbe@cisco.com>
2021-08-23 18:03:26 -0500

    Add devices file and the scripts for video2

---
+++ video2_cap.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+"""
+Script para mostrar como obtener las capabilities de NETCONF en los equipos del laboratorio
+Autor: Raul Gomez
+email: raul.agobe@gmail.com
+"""
+
+from ncclient import manager
+import devices as d
+
+
+# Funcion para conectarse via NETCONF a un equipo de red y obtener los capabilities
+
+def get_capabilities(device):
+
+    with manager.connect(host=device['address'], 
+                            port=device['port'],
+                            username=device['username'],
+                            password=device['password'],
+                            hostkey_verify=False) as m:
+
+        cap = m.server_capabilities
+    
+    return cap
+
+# Llamamos a la funcion get_capabilities e imprimimos el resultado en la terminal
+
+if __name__ == '__main__':
+
+    device = d.lab_4331
+
+    cap = get_capabilities(device)
+
+    print('Aqui los capabilities: ')
+    for line in cap:
+        print(line)
+    print('')
