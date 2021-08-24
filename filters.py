e70726d7ca4584e87d6d531646e6b5ae2511a509
Raul Gomez <rgomezbe@cisco.com>
2021-08-24 14:07:31 -0500

    Add video3.py and filters.py

---
+++ filters.py
@@ -0,0 +1,30 @@
+hostname = '''
+<native xmlns='http://cisco.com/ns/yang/Cisco-IOS-XE-native'>
+    <hostname></hostname>
+</native>
+'''
+
+usernames = '''
+<native xmlns='http://cisco.com/ns/yang/Cisco-IOS-XE-native'>
+    <username></username>
+</native>'''
+
+routes = '''
+<native xmlns="http://cisco.com/ns/yang/Cisco-IOS-XE-native">
+    <ip>
+        <route>
+            <ip-route-interface-forwarding-list>
+            </ip-route-interface-forwarding-list>
+        </route>
+    </ip>
+</native>'''
+
+loopback10 = '''
+<native xmlns='http://cisco.com/ns/yang/Cisco-IOS-XE-native'>
+    <interface>
+        <Loopback>
+            <name>10</name>
+        </Loopback>
+    </interface>
+</native>
+'''
---
+++ video3.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+"""
+Script para mostrar como obtener la running config de los equipos de laboratorio usando filtros
+Autor: Raul Gomez
+email: raul.agobe@gmail.com
+"""
+
+from ncclient import manager
+import devices as d
+import filters as f
+
+# Funcion para conectarse via NETCONF a un equipo de red y obtener la running config usando filtros
+
+def get_filtered_config(device,netconf_filter):
+
+    with manager.connect(host=device['address'], 
+                            port=device['port'],
+                            username=device['username'],
+                            password=device['password'],
+                            hostkey_verify=False) as m:
+
+        if float(device['version']) >= 17.3:
+            filtered_config = m.get_config('running',filter=('subtree',netconf_filter))
+
+        else:
+            updated_filter = '<filter>' + netconf_filter + '</filter>'
+            filtered_config = m.get_config('running',updated_filter)
+    
+    return filtered_config
+
+# Llamamos a la funcion get_running_config e imprimimos el resultado en la terminal
+
+if __name__ == '__main__':
+
+    config = get_filtered_config(d.lab_4331,f.usernames)
+
+    print('Aqui la configuracion: ')
+    print(config)
+    print('')
